c99d7dd25cd3f9ae39571436fb2d8ae2e2f51a8a
onee-only <kimww0306@gmail.com>
2024-01-30 16:43:25 +0900

    add :: domain 모델 정의

---
+++ internal/domain/random_chat.go
@@ -0,0 +1,70 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+)
+
+// RevealStage indicates the status of random chat's reveal stage.
+type RevealStage uint8
+
+const (
+	// RevealStageInit indicates nobody hasn't voted for the reveal.
+	RevealStageInit RevealStage = iota
+
+	// RevealStageWaiting indicates at least 1 person voted for the reveal.
+	RevealStageWaiting
+
+	// RevealStageWaiting indicates all people voted for the reveal.
+	RevealStageRevealed
+)
+
+// RevealVote is user's vote to the reveal.
+type RevealVote struct {
+	UserID    uuid.UUID
+	CreatedAt uuid.UUID
+}
+
+// RandomChat is chat generated randomly. it includes chat in it.
+type RandomChat struct {
+	Chat
+	Stage       RevealStage
+	RevealVotes []RevealVote
+	aliases     map[uuid.UUID]string
+}
+
+// SetAliases sets alias hashmap into given hashamp.
+// Once it is set, it should be immutable.
+func (rc *RandomChat) SetAliases(aliases map[uuid.UUID]string) {
+	tmp := make(map[uuid.UUID]string, len(aliases))
+	for k, v := range aliases {
+		tmp[k] = v
+	}
+	rc.aliases = tmp
+}
+
+// GetAlias maps userID into specified alias.
+func (rc *RandomChat) GetAlias(userID uuid.UUID) string {
+	alias, ok := rc.aliases[userID]
+	if !ok {
+		// TODO: Change this to make it fit into requirements.
+		return "Unknown"
+	}
+	return alias
+}
+
+// CanVote checks wheter user can vote for reveal in the chat.
+func (rc *RandomChat) CanVote(userID uuid.UUID) bool {
+	if rc.IsVoteFinished() {
+		return false
+	}
+	for _, vote := range rc.RevealVotes {
+		if vote.UserID == userID {
+			return false
+		}
+	}
+	return true
+}
+
+func (rc *RandomChat) IsVoteFinished() bool {
+	return len(rc.UserIDs) == len(rc.RevealVotes)
+}
