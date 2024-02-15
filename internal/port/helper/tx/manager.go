c568343eb7a80247dc91b9bcb1bf0a6a754736f7
onee-only <kimww0306@gmail.com>
2024-02-15 23:15:03 +0900

    update :: tx evalute에 panic recovery 추가

--- internal/port/helper/tx/manager.go
+++ internal/port/helper/tx/manager.go
@@ -3,6 +3,7 @@ package tx
 import (
 	"context"
 	"errors"
+	"fmt"
 )
 
 type Manager struct{}
@@ -13,10 +14,21 @@ func (m *Manager) Begin(ctx context.Context) context.Context {
 	return context.WithValue(ctx, txKey{}, &utx)
 }
 
-// Evaluate decides whether to commit or roll back the transaction depending on the state of the error.
+// Evaluate decides whether to commit or rollback the transaction depending on the state of the error.
+// When it meets panic, it rollbacks the transaction and produces panic again.
 // If execution of commit or rollback fails, it appends its error to given error.
 // It is recommended to call it as deferred statement right after the excution of Begin.
 func (m *Manager) Evaluate(ctx context.Context, err *error) {
+	if e := recover(); e != nil {
+		err := fmt.Errorf("panic during transaction: %v", err)
+
+		m.doEvaluate(ctx, &err)
+		panic(err)
+	}
+	m.doEvaluate(ctx, err)
+}
+
+func (m *Manager) doEvaluate(ctx context.Context, err *error) {
 	utx := Extract(ctx)
 	if *err == nil {
 		*err = utx.commit(ctx)
